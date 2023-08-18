b1cf775a1b9b6ffbb425af1e55c1772e35406939
rtlsdrblog <rtlsdrblog@gmail.com>
2023-08-18 21:28:53 +1200

    fix bias tee function call

--- src/librtlsdr.c
+++ src/librtlsdr.c
@@ -1244,7 +1244,7 @@ int rtlsdr_set_offset_tuning(rtlsdr_dev_t *dev, int on)
 	/* RTL-SDR-BLOG Hack, enables us to turn on the bias tee by clicking on "offset tuning" in software that doesn't have specified bias tee support.
     * Offset tuning is not used for R820T/R828D devices so it is no problem.
 	*/
-	rtlsdr_set_gpio(dev, 0, on);
+	rtlsdr_set_bias_tee(dev, on);
 
 	if ((dev->tuner_type == RTLSDR_TUNER_R820T) ||
 	    (dev->tuner_type == RTLSDR_TUNER_R828D))
